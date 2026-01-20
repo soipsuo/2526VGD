bb3e967670110abc865d77e9bc14cf6c6c8f3ef1
Adam Hamdan <an.hamdan2009@gmail.com>
2026-01-19 22:08:06 -0500

    Chase Scene Update & Kill Brick

---
+++ Assets/Scripts/killBrick.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class killBrick : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject killUI;
+    public GameObject killnumbers1;
+    public GameObject killnumbers2;
+    public GameObject killnumbers3;
+    public float waitTime;
+    private float timer = 0f;
+    private bool playerCollided = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player collided with kill brick.");
+            player.SetActive(false);
+            killUI.SetActive(true);
+            playerCollided = true;
+
+
+        }
+    }
+
+    private void Update()
+    {
+        if (playerCollided)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1f && !killnumbers1.activeSelf)
+            {
+                killnumbers1.SetActive(true);
+            }
+
+            if (timer >= 2f && !killnumbers2.activeSelf)
+            {
+                killnumbers2.SetActive(true);
+            }
+
+            if (timer >= 3f && !killnumbers3.activeSelf)
+            {
+                killnumbers3.SetActive(true);
+            }
+            if (timer >= waitTime)
+            {
+                timer = 0f;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
