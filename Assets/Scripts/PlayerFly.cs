72b4e9cda949cc905ad3c938d0fb8a8fd475948c
NinjaDuck200 <neelabh.das@gmail.com>
2026-02-03 22:52:04 -0500

    Room 3 Tilemaps + Flying Animations

--- Assets/Scripts/PlayerFly.cs
+++ Assets/Scripts/PlayerFly.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
-
 public class PlayerFly : MonoBehaviour
 {
     public GameObject player;
     private Rigidbody2D rb;
+    private Animator _anim; // Added reference
 
     public float flapStrength = 8f;
     public float forwardSpeed = 3f;
@@ -17,22 +17,33 @@ public class PlayerFly : MonoBehaviour
     void Start()
     {
         rb = player.GetComponent<Rigidbody2D>();
+        // Grab the animator from the player (or its child visuals)
+        _anim = player.GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        // 1. Detect the "Press" (Flap starts)
         if (Input.GetKeyDown(KeyCode.Space) && flightready)
         {
             flapQueued = true;
             flightready = false;
+
+            if (_anim != null) _anim.SetBool("isFlapping", true); // Turn on wings-up
+
             StartCoroutine(flapCooldown());
+        }
+
+        // 2. Detect the "Release" (Go back to gliding)
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (_anim != null) _anim.SetBool("isFlapping", false); // Turn off wings-up
         }
     }
 
     void FixedUpdate()
     {
         Vector2 vel = rb.linearVelocity;
-
         vel.x = forwardSpeed;
 
         if (flapQueued)
